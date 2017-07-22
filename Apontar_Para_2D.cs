ecad0d41e730b853bd7ab70527f610e6a188eec1
Andre_LA <andrluiz.alvares.alvares84@gmail.com>
2017-07-21 21:54:18 -0300

    gitignore atualizado e novos scripts: Apontar_Para_2D.cs : Aponta para uma certa direção, apenas para 2D Correr.cs : Movimenta certo Transform para alguma direção baseado em eixos

---
+++ Apontar_Para_2D.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+
+public class Apontar_Para_2D : MonoBehaviour {
+    #if UNITY_EDITOR
+
+    [Header("Para o Editor")]
+    [Tooltip("Isso desenhará uma linha (Gizmos) entre o apontador e o alvo")]
+    public bool desenharLinhas = true;
+    [Tooltip("Cor da linha entre os dois")]
+    public Color corEntreOsDois = Color.red;
+    [Tooltip("Cor da rotação do apontador")]
+    public Color corDaRotacao = Color.red;
+
+    #endif
+
+    [Header("Componentes Transforms")]
+	[Tooltip("Transform do Gbj que vai apontar ao alvo")]
+    public Transform apontador;
+    [Tooltip("Transform do alvo que o apontador vai apontar")]
+    public Transform alvo;
+    
+    [Header("Propriedades modificadoras")]
+    [Tooltip("Se for > 0, então a quantidade de ângulos possíveis será a abaixo")]
+    public int limiteAngulos;
+    [Tooltip("Use isso para adicionar na rotação resultante (assim mudará qual a ponta do apontador)")]
+    public float rotacaoExtra;
+
+    void Update() {
+        float angulo = Obtem_Angulo_Para_Apontar();
+        if (limiteAngulos > 0) {
+            float pedaco_angulo = 360f / limiteAngulos;
+            for (int i = 0; i <= limiteAngulos; i++) {
+                if (angulo > i * pedaco_angulo - pedaco_angulo/2)
+                    Aplica_Rotacao(i * pedaco_angulo + rotacaoExtra);
+            }            
+        } else {
+            angulo += rotacaoExtra;
+            Aplica_Rotacao(angulo);
+        }
+    }
+
+    void Aplica_Rotacao(float angulo) {
+        apontador.rotation = Quaternion.Euler(0, 0, angulo);
+    }
+
+    float Obtem_Angulo_Para_Apontar() {
+        Vector2 posicao_relativa = new Vector2(alvo.position.x - apontador.position.x, alvo.position.y - apontador.position.y);
+        posicao_relativa.Normalize();
+            
+        float angulo_180 = Vector2.Angle(new Vector2(1, 0), posicao_relativa);
+
+        if (posicao_relativa.y < 0)
+            angulo_180 = 360f - angulo_180;
+            
+        return angulo_180;
+    }
+
+    #if UNITY_EDITOR
+
+    void OnDrawGizmos() {
+        if (desenharLinhas && alvo && apontador) {
+            Gizmos.color = corEntreOsDois;
+            Gizmos.DrawLine(apontador.position, alvo.position);
+            Gizmos.DrawWireSphere(apontador.position, 0.5f);
+
+            Gizmos.color = corDaRotacao;
+            Vector3 direcao = new Vector3( Mathf.Cos(apontador.eulerAngles.z * Mathf.Deg2Rad), Mathf.Sin(apontador.eulerAngles.z * Mathf.Deg2Rad) );
+            Gizmos.DrawRay(apontador.position, direcao);
+        }
+    }
+
+    #endif
+}
---
+++ Correr.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+
+public class Correr : MonoBehaviour {
+
+	#if UNITY_EDITOR
+
+	[Header("Para o Editor")]
+    [Tooltip("Isso desenhará uma linha (Gizmos) entre o apontador e o alvo")]
+    public bool desenharLinhas = true;
+    [Tooltip("Cor da seta de movimento")]
+    public Color corDaSeta = Color.red;
+
+	#endif
+
+	[Header("Componente Transform")]
+	[Tooltip("Os valores de posição desse Transform que serão modificados ao correr")]
+	public Transform corredor;
+	
+	[Header("Eixos e velocidade")]
+	[Tooltip("Velocidade de movimento")]	
+	public float velocidade;
+
+	[Tooltip("São os eixos para o Input.GetAxis")]
+	public string eixoParaX, eixoParaY, eixoParaZ;
+	
+	[Tooltip("Isso ativa o uso de Input.GetRawAxis ao invés de Input.GetAxis, a direfença é que não há aquela suavização quando solta/aperta")]
+	public bool usarEixosPuros;
+	
+	[Tooltip("World é relativo ao mundo, Self é é local")]
+	public Space relativoAo;
+	
+	[Header("Trancas e Bloqueios")]	
+	[Tooltip("Esse eixo não será movimentado")]
+	public bool bloquearX, bloquearY, bloquearZ;
+		
+	void Update () {
+		Movimentar();
+	}
+
+	public void Movimentar() {
+		float final_x = bloquearX 
+						? 0
+						: (usarEixosPuros
+							? Input.GetAxisRaw(eixoParaX)
+							: Input.GetAxis(eixoParaX)) * Time.deltaTime * velocidade;
+		float final_y = bloquearY
+						? 0
+						: (usarEixosPuros
+							? Input.GetAxisRaw(eixoParaY)
+							: Input.GetAxis(eixoParaY)) * Time.deltaTime * velocidade;
+		float final_z = bloquearZ
+						? 0
+						: (usarEixosPuros
+							? Input.GetAxisRaw(eixoParaZ)
+							: Input.GetAxis(eixoParaZ)) * Time.deltaTime * velocidade;
+
+		corredor.Translate(final_x, final_y, final_z, relativoAo);
+	}
+
+	#if UNITY_EDITOR
+
+	void OnDrawGizmos() {
+		if (desenharLinhas && corredor) {
+			Gizmos.color = corDaSeta;
+
+			float final_x = bloquearX 
+							? 0
+							: (usarEixosPuros
+								? Input.GetAxisRaw(eixoParaX)
+								: Input.GetAxis(eixoParaX)) * Time.deltaTime * velocidade;
+			float final_y = bloquearY
+							? 0
+							: (usarEixosPuros
+								? Input.GetAxisRaw(eixoParaY)
+								: Input.GetAxis(eixoParaY)) * Time.deltaTime * velocidade;
+			float final_z = bloquearZ
+							? 0
+							: (usarEixosPuros
+								? Input.GetAxisRaw(eixoParaZ)
+								: Input.GetAxis(eixoParaZ)) * Time.deltaTime * velocidade;
+
+			Vector3 direcao = new Vector3(final_x, final_y, final_z);
+			direcao.Normalize();
+			Gizmos.DrawRay(corredor.position, direcao);
+			Gizmos.DrawWireSphere(corredor.position + direcao, 0.1f);
+		}
+	}
+
+	#endif
+}
